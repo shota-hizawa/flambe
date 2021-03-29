60450c517f7970cfec4aaaedd37b7a5d44f132c1
日沢翔太 <hshota28@gmail.com>
2021-03-30 01:55:58 +0900

    [FLAMBE-8]feat: 特定ユーザに紐づく未完了タスクを表示するAPIを追加

--- app/server/controllers/user_controller.py
+++ app/server/controllers/user_controller.py
@@ -2,6 +2,7 @@ from services import user_service
 from database import get_db
 from fastapi import APIRouter, Depends
 from schemas.user_schema import UserSchemaInDB, CreateUserSchema
+from schemas.task_schema import TaskSchemaInDB
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -9,17 +10,26 @@ router = APIRouter()
 
 
 @router.get("/", response_model=List[UserSchemaInDB])
-async def get_all(db: Session = Depends(get_db)):
+async def get_all_users(db: Session = Depends(get_db)):
     return user_service.get_all(db)
 
 
+@router.get("/{user_id}/tasks/incomplete", response_model=List[TaskSchemaInDB])
+async def get_incomplete_tasks_assigned_to_user(
+    user_id: int, db: Session = Depends(get_db)
+):
+    return user_service.get_incomplete_tasks(db=db, user_id=user_id)
+
+
 @router.post("/", response_model=UserSchemaInDB)
-async def create(create_user_schema: CreateUserSchema, db: Session = Depends(get_db)):
+async def create_user(
+    create_user_schema: CreateUserSchema, db: Session = Depends(get_db)
+):
     return user_service.create(
         db, create_user_schema.username, create_user_schema.password
     )
 
 
 @router.delete("/{user_id}", response_model=None)
-async def delete(user_id: int, db: Session = Depends(get_db)):
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
     user_service.delete(db, user_id)
--- app/server/services/user_service.py
+++ app/server/services/user_service.py
@@ -2,12 +2,26 @@ from repositories import user_repository
 from sqlalchemy.orm import Session
 from utils.crypt import encrypt
 from typing import List, cast
-from entities import User
+from entities import User, Task
+from entities.task import Status
 from exceptions import *
+from utils.sort import sort_tasks_by_priority
 
 
 def get_all(db: Session) -> List[User]:
     return user_repository.find_all(db=db)
+
+
+def get_incomplete_tasks(db: Session, user_id: int) -> List[Task]:
+    user = get_user_by_id(db=db, user_id=user_id)
+
+    doing_tasks = sort_tasks_by_priority(
+        list(filter(lambda task: task.status is Status.DOING, user.tasks))
+    )
+    todo_tasks = sort_tasks_by_priority(
+        list(filter(lambda task: task.status is Status.TODO, user.tasks))
+    )
+    return [*doing_tasks, *todo_tasks]
 
 
 def create(db: Session, username: str, password: str) -> User:
